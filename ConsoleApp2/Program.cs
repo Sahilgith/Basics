44010408747c848dbba64a0a7af737ace966be5b
Sahil Mohite <sahilmohite169@gmal.com>
2025-04-04 12:21:30 +0530

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices;
+
+
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*  int number = 1;
+              Console.WriteLine(number);
+
+              float f = 10.15f;
+              Console.WriteLine(f);       
+
+              char c = 'a';
+              string s = "Mrash";
+
+              Console.WriteLine("{0} {1}"  , byte.MinValue, byte.MaxValue);
+              Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+              const float pi = 3.14f;
+            */
+
+            //type conversion
+
+
+            //Console.WriteLine("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            //Console.WriteLine("How many candies do you want");
+            //string cand = Console.ReadLine();
+            //Console.WriteLine("You will get 4 more: " + (Convert.ToInt32(cand) + 4));
+
+            //operators
+
+
+            //string line = "Hello world this is sahil";
+            //Console.WriteLine(line.Length);
+            //Console.WriteLine(line.ToUpper());
+            //Console.WriteLine(line.ToLower());
+
+
+            //string name = Console.ReadLine();
+            //string candies = Console.ReadLine();
+
+            //Console.WriteLine($"your name  is {name}. your candies is {candies}");
+            for(int i=1; i<5; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+
+
+
+
+
+
+            
+            
+
+
+
+
+
+
+
+        }
+    }
+}
